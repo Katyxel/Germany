8997b34e64a934ef0b2097d305b7a96b58f71913
Екатерина Щукина <ekat.writer@gmail.com>
2024-03-27 17:10:59 +0300

    Работа над футером

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,81 +1,48 @@
 import React from "react";
-import styles from "./Footer.module.css";
+// import Script from 'next/script';
 
 const Footer = () => {
   return (
-    <footer className={`bg-gray-100 text-gray-800 p-8 ${styles.footer}`}>
-      <div className="flex flex-wrap justify-around container">
-        <div className="mb-4">
-          <h2 className="text-lg font-semibold mb-2">Колонка 1</h2>
-          <a
-            href="#"
-            className="text-gray-800 hover:text-blue-500 transition duration-300 block mb-1"
-          >
-            Ссылка 1
-          </a>
-          <a
-            href="#"
-            className="text-gray-800 hover:text-blue-500 transition duration-300 block mb-1"
-          >
-            Ссылка 2
-          </a>
-        </div>
-
-        <div className="mb-4">
-          <h2 className="text-lg font-semibold mb-2">Колонка 2</h2>
-          <a
-            href="#"
-            className="text-gray-800 hover:text-blue-500 transition duration-300 block mb-1"
-          >
-            Ссылка 1
-          </a>
-          <a
-            href="#"
-            className="text-gray-800 hover:text-blue-500 transition duration-300 block mb-1"
-          >
-            Ссылка 2
-          </a>
-        </div>
-
-        <div className="mb-4">
-          <h2 className="text-lg font-semibold mb-2">Колонка 3</h2>
-          <a
-            href="#"
-            className="text-gray-800 hover:text-blue-500 transition duration-300 block mb-1"
-          >
-            Ссылка 1
-          </a>
-          <a
-            href="#"
-            className="text-gray-800 hover:text-blue-500 transition duration-300 block mb-1"
-          >
-            Ссылка 2
-          </a>
-        </div>
+    <div className="container my-4">
+    <footer className="bg-gray-100 my-2 text-gray-800 p-8 flex flex-col items-center">
+      <div className="mb-8 text-center">
+        Помощь в иммиграции в Германию и жизнь в ФРГ © 2008 - 2024 Пётр Иванов.
+        При использовании любых материалов сайта, включая фотографии и тексты,
+        активная ссылка на источник обязательна. Наш офис в России: Москва,
+        Научный переулок, 20, подъезд 1, этаж 2, оф. 205, бизнес-центр «Филион»· +7 495 142 17 55
       </div>
 
-      {/* Ссылки на социальные сети */}
-      <div className="mt-10 flex justify-center">
+      {/* <div className="mb-8 flex justify-center">
+        <div className="map-container">
+          <Script
+            src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A7bbce5305b67a70c787ade7c1eead755788de0eb6b037a2e22437a2bb8a3dcac&amp;width=569&amp;height=374&amp;lang=ru_RU&amp;scroll=true"
+            strategy="afterInteractive"
+          />
+        </div>
+      </div> */}
+
+      <div className="flex justify-center">
         <a
           href="#"
-          className="text-orange-600 hover:text-blue-500 transition duration-300 mr-12"
+          className="text-orange-600 hover:text-blue-500 transition duration-300 mr-4"
         >
           Facebook
         </a>
         <a
           href="#"
-          className="text-orange-600 hover:text-blue-500 transition duration-300 mr-12"
+          className="text-orange-600 hover:text-blue-500 transition duration-300 mr-4"
         >
           Twitter
         </a>
         <a
           href="#"
-          className="text-orange-600 hover:text-blue-500 transition duration-300 mr-12"
+          className="text-orange-600 hover:text-blue-500 transition duration-300 mr-4"
         >
           Instagram
         </a>
       </div>
     </footer>
+    </div>
   );
 };
 
